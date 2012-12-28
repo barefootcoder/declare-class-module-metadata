8d7ec06168968a8492949e12753b4f50b6d66ba5
Buddy Burden <barefootcoder@gmail.com>
2012-12-27 18:03:54 -0800

    skeleton distro

---
+++ lib/Declare/Class/Module/Metadata.pm
@@ -0,0 +1,14 @@
+package Declare::Class::Module::Metadata;
+
+use strict;
+use warnings;
+
+# VERSION
+
+
+1;
+
+# ABSTRACT: Like Module::Metadata, but understands class declarations
+# COPYRIGHT
+
+__END__
